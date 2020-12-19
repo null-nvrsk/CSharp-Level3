d54f50aea57653b665b52fd81cd738da1e16d18a
NULL <nullnvrsk@gmail.com>
2020-12-19 19:38:27 +0300

    2nd variant of validation (user validation)

--- MailSenderWPF/Models/Recipient.cs
+++ MailSenderWPF/Models/Recipient.cs
@@ -1,21 +1,41 @@
 ﻿using MailSenderWPF.Models.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MailSenderWPF.Models
 {
-    public class Recipient : Person
+    public class Recipient : Person, IDataErrorInfo
     {
-        public override string Name { get => base.Name;
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "Name")
+                {
+                    if (Name == "") return "Пустое имя";
+                    if (Name.Length < 3) return "Слишком короткое имя";
+                }
+
+                return "";
+            }
+
+	    }
+
+        public string Error => throw new NotImplementedException();
+
+        public override string Name 
+        {
+            get => base.Name;
             set
             {
-                if (value == "")
-                    throw new ArgumentException("Пустое имя", nameof(value));
-                if (value.Length < 3)
-                    throw new ArgumentException("Слишком короткое имя", nameof(value));
+                //if (value == "")
+                //    throw new ArgumentException("Пустое имя", nameof(value));
+                //if (value.Length < 3)
+                //    throw new ArgumentException("Слишком короткое имя", nameof(value));
 
                 base.Name = value;
             }
